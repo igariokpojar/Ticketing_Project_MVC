d3f64b484cba8ab550691c5856040faadf285eaf
Igariok <igariokpojar@yahoo.com>
2022-09-29 15:03:29 -0500

    Ticketing Part-1

---
+++ src/main/java/com/cydeo/service/impl/AbstractMapService.java
@@ -0,0 +1,25 @@
+package com.cydeo.service.impl;
+
+
+import java.util.*;
+
+public abstract class AbstractMapService <T,ID>{
+
+    public Map<ID,T> map = new HashMap<>(); //DB (ID-key) and (T-value)
+
+     T save(ID id, T object){
+         map.put(id,object);
+         return object;
+     }
+
+     List<T> findAll(){
+         return new ArrayList<>(map.values());
+     }
+
+     T findById(ID id){
+         return map.get(id);
+     }
+     void deleteById(ID id){
+         map.remove(id);
+     }
+}
