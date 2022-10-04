fe66a98072891e60c79303dc3f8e0e37802ad866
Igariok <igariokpojar@yahoo.com>
2022-10-04 14:06:04 -0500

    Ticketing Part-5

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -7,6 +7,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,5 +43,24 @@ public class TaskController {
         taskService.save(task); // ask your self if we have Service?
 
          return "redirect:/task-create";
+    }
+
+    @GetMapping("/task/delete/{id}") // go to HTML and see what they put here for delete //task/delete/{id}
+    public String deleteTask(@PathVariable("id") Long id){
+
+        taskService.deleteById(id);
+
+        return "redirect:/task-create";
+    }
+
+    @GetMapping("/update/{taskId}") // go to create.html and find the Button Update
+    public String editTask(@PathVariable("taskId") Long taskId,Model model) {
+        // pass all the Object which is coming from Service
+        model.addAttribute("task",taskService.findById(taskId));
+        model.addAttribute("projects",projectService.findAll());
+        model.addAttribute("employees",userService.findEmployees());
+        model.addAttribute("tasks",taskService.findAll());
+
+        return "task/update";
     }
 }
